b25a3a3c884ba3f7e6a03efb1ebde4a2c02b8a51
chaochen0918 <walton860918@gmail.com>
2025-07-29 15:48:41 +0800

    add standard process for EDA

---
+++ src/ML_Pipeline/eda.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# -----------------------------------------------------------------------------------
+# connect to the database
+# -----------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------
+# load all tables from the database into pandas DataFrames
+# -----------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------
+# Exploratory Data Analysis
+# -----------------------------------------------------------------------------------
+# Goals:
+# - Have a general idea of how the database looks like
+# - Remove unnessary columns
+# - Check if any missing values exist
+# - Check if abnormal values exist (extreme large or small value ?)
+# - Identify patterns for the potential usage of later predictive modeling
+#   - CAN BE MORE SPECIFIC, e.g., show the distribution of variables, etc.
+# - Generate ideas for feature engineering
+
+
+# ----------------------------------------------
+# Table 1
+# ----------------------------------------------
+"""
+TABLE SUMMARY
+- Table Name: TO BE FILLED
+- Brief Description: TO BE FILLED
+- Sample Size: TO BE FILLED (add a note when the size changes)
+- Relationship w/ Other Tables: TO BE FILLED (current table id : other table matched id)
+- Missing Values Columns (type): counts
+    - TO BE FILLED
+- Abnormal Values Columns (type): reason
+    - TO BE FILLED
+- Remove Unnecessary Columns: reason
+    - TO BE FILLED
+
+COLUMNS TYPES
+1. Primary Key:
+    - TO BE FILLED
+2. Foreign Key:
+    - TO BE FILLED
+3. Numeric:
+    - TO BE FILLED
+4. Categorical:
+    - TO BE FILLED
+5. Others:
+    - TO BE FILLED
+"""
+# data overview
+# df.head()
+# df.info()
+# df.describe() 
+
+# exploratory data analysis
+# data distribution
+# TO BE FILLED
+# scatter plot
